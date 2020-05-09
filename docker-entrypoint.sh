48ef1415e371d8c49a98a6256d812860f13a59af
Ethan Robish <ethan.robish@gmail.com>
2020-05-09 00:41:30 -0500

    Think I fixed the log rotation issue.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# exit script if an error is encountered
 set -e
+
+# do final log rotation
+stop() {
+	echo "Stopping zeek..."
+	zeekctl stop
+	trap - SIGINT SIGTERM
+	exit
+}
 
 # run zeekctl diag on error
 diag() {
@@ -8,21 +17,24 @@ diag() {
 	zeekctl diag
 	trap - ERR
 }
-trap 'diag' ERR
 
-# do final log rotation
-stopzz() {
-	echo "Stopping zeek..."
-	zeekctl stop
-	trap - SIGKILL SIGINT SIGTERM
-}
-trap 'stop' SIGKILL SIGINT SIGTERM
+trap 'diag' ERR
 
 # ensure Zeek has a valid, updated config, and then start Zeek
 zeekctl check
 zeekctl install
 zeekctl start
 
-# periodically run the Zeek cron monitor
+# ensure spool logs are rotated when container is stopped
+trap 'stop' SIGINT SIGTERM
+
+# periodically run the Zeek cron monitor to restart any terminated processes
 zeekctl cron enable
-crond -f -L /dev/fd/1
+# disable the zeekctl ERR trap as there are no more zeek commands to fail
+trap - ERR
+
+# daemonize cron but log output to stdout
+crond -b -L /dev/fd/1
+
+# infinite loop to prevent container from exiting and allow processing of signals
+while :; do sleep 1s; done
